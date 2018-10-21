6a60f3e08858f773d41ca1277a5aea300a782705
Teddy Cornaut <tcornaut@cadoles.com>
2018-10-21 12:05:21 +0200

    Dataset de test complet

--- scripts/initData.sh
+++ scripts/initData.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Wipe des données existantes
+killall modd
+killall bloodcoin-server
 rm *.db
 ./bloodcoin-cli dump
 cp bloodcoin.db bloodcoin-server.db
@@ -9,9 +11,29 @@ cp bloodcoin.db bloodcoin-server.db
 ./bloodcoin-server &
 sleep 2
 
+peer=http://localhost:3000/blocks/new
+
 # Insertion d'un jeu de données
 prescription=$(cat sample/valid_prescription.json)
-echo $prescription
-block=$(./bloodcoin-cli -peer http://localhost:3000/blocks/new prescription -data "$prescription" | sed 's/^.*Block: \(.*\){34}/\1/')
-echo $block
-#./bloodcoin-cli prescription -data "$(cat sample/valid_prescription.json)"
+block=$(./bloodcoin-cli -peer $peer prescription -data "$prescription" -quiet)
+./bloodcoin-cli -peer $peer notification -prescription $block -type "received" -firstName "Jean" -lastName "Dupont" -service "CHU Dijon - Réanimation"
+./bloodcoin-cli -peer $peer notification -prescription $block -type "packaging" -firstName "Corrine" -lastName "Touzet" -service "EFS Dijon - Stocks PSL"
+./bloodcoin-cli -peer $peer notification -prescription $block -type "packaged" -firstName "Corrine" -lastName "Touzet" -service "EFS Dijon - Stocks PSL"
+./bloodcoin-cli -peer $peer notification -prescription $block -type "delivering" -firstName "Speedy" -lastName "Gonzales" -service "Livraison 3000 Dijon"
+./bloodcoin-cli -peer $peer notification -prescription $block -type "delivered" -firstName "Speedy" -lastName "Gonzales" -service "Livraison 3000 Dijon"
+./bloodcoin-cli -peer $peer notification -prescription $block -type "transfused" -firstName "Michel" -lastName "Plancard" -service "CHU Dijon - Chirurgie"
+
+prescription=$(cat sample/prescription_low.json)
+block=$(./bloodcoin-cli -peer $peer prescription -data "$prescription" -quiet)
+./bloodcoin-cli -peer $peer notification -prescription $block -type "received" -firstName "Jean" -lastName "Dupont" -service "CHU Dijon - Réanimation"
+./bloodcoin-cli -peer $peer notification -prescription $block -type "packaging" -firstName "Corrine" -lastName "Touzet" -service "EFS Dijon - Stocks PSL"
+./bloodcoin-cli -peer $peer notification -prescription $block -type "packaged" -firstName "Corrine" -lastName "Touzet" -service "EFS Dijon - Stocks PSL"
+
+prescription=$(cat sample/prescription_high.json)
+block=$(./bloodcoin-cli -peer $peer prescription -data "$prescription" -quiet)
+./bloodcoin-cli -peer $peer notification -prescription $block -type "received" -firstName "Pierre" -lastName "Ponce" -service "CHU Besançon - Hématologie"
+./bloodcoin-cli -peer $peer notification -prescription $block -type "packaging" -firstName "Martin" -lastName "Bouygues" -service "EFS Besançon - Stocks PSL"
+
+prescription=$(cat sample/prescription_emergency.json)
+block=$(./bloodcoin-cli -peer $peer prescription -data "$prescription" -quiet)
+./bloodcoin-cli -peer $peer notification -prescription $block -type "received" -firstName "Pierre" -lastName "Ponce" -service "CHU Besançon - Hématologie"
